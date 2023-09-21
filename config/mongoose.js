ac6cbd9666c6771912fb4a06454a30f34d409758
jayzadafiya <zadafiyajay2@gmail.com>
2023-09-21 22:53:53 +0530

    hosting

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const DB = 'mongodb+srv://zadafiyajay2:P0ZQe0e5wdiswHAx@cluster0.7ttgdye.mongodb.net/?retryWrites=true&w=majority'
+
+// These set of line can be written in async await fashion, but I have followed the documentation. 
+mongoose.connect(DB).then(()=>{
+     console.log('connection successful');
+ }).catch((err) => console.log("no connection " + err));
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open',  function(){
+     console.log('Connected to Database :: MongoDB');
+});
+
+ 
+module.exports = db;  
